61d75882450fb197c3bd8cfb09832417a6628d76
M1stakesWereMade <superlexlive@gmail.com>
2024-07-23 15:18:45 +0300

    Add files via upload

---
+++ dz.py
@@ -0,0 +1,16 @@
+def test_function():
+    print("Вызов test_function")
+
+    def inner_function():
+        print("Я в области видимости функции test_function")
+
+    inner_function()
+
+    # Попытка вызова inner_function вне test_function
+    try:
+        inner_function()
+    except NameError as e:
+        print(f"Ошибка при вызове inner_function: {e}")
+
+if __name__ == "__main__":
+    test_function()
